fe84bf987b404c006fd74846f90aa46fedcf56a5
Shih Hsuan Yu <kevin0921799052@gmail.com>
2023-10-02 19:26:23 +0800

    modify app.py allow user input number

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-Human = type(
-    "Human",
-    (object,),
-    {
-        "name": "Max",
-    },
-    
-)
-print()
+def addnum(a,b):
+    return a+b
+
+x = int(input("Please input number1 : "))
+y = int(input("Please input number2 : "))
+print(addnum(x,y))
